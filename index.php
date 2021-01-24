d255406f3c906cad84a024c989350a0e47bdca3c
maksiz32 <maks-manzulin@mail.ru>
2021-01-24 20:30:10 +0300

    1,2,3,4,5,7,8,9 plus start create render over layout

--- index.php
+++ index.php
@@ -1,12 +1,67 @@
 <?php
+define('TEMPLATES_DIR', 'templates/');
+define('LAYOUTS_DIR', 'layouts/');
 
-    $menu = renderTemplate('menu');
-    $content = renderTemplate('about');
-    echo renderTemplate('layout', $content, $menu);
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 'index';
+}
 
-    function renderTemplate($page, $content = "", $menu = "") {
-        ob_start();
-        include $page . ".php";
-        return ob_get_clean();
+$params = [
+    'count' => 2
+];
+
+switch ($page) {
+    case 'index':
+        $params['name'] = 'Alex';
+        break;
+
+    case 'catalog':
+        $params['catalog'] = getCatalog();
+        break;
+
+    case 'apicatalog':
+        echo json_encode(getCatalog(), JSON_UNESCAPED_UNICODE);
+        die();
+}
+
+
+echo render($page, $params);
+
+function getCatalog() {
+    return [
+        [
+            'name' => 'Пицца',
+            'price' => 24
+        ],
+        [
+            'name' => 'Чай',
+            'price' => 1
+        ],
+        [
+            'name' => 'Яблоко',
+            'price' => 12
+        ],
+    ];
+}
+
+function render($page, $params = []) {
+    return renderTemplate(LAYOUTS_DIR . 'main', [
+        'menu' => renderTemplate('menu', $params),
+        'content' => renderTemplate($page, $params)
+    ]);
+}
+
+function renderTemplate($page, $params = []) {
+    extract($params);
+//    foreach ($params as $key => $value) {
+//        $$key = $value;
+//    }
+    ob_start();
+    $fileName = TEMPLATES_DIR . $page . ".php";
+    if (file_exists($fileName)) {
+        include $fileName;
     }
-
+    return ob_get_clean();
+}
