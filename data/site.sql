5d6bfe71710f66dde5349942c4996f7d8f72dfb9
maksiz32 <maks-manzulin@mail.ru>
2021-02-06 03:07:22 +0300

    Lesson 6. dump SQL-db

--- data/site.sql
+++ data/site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Фев 01 2021 г., 12:37
+-- Время создания: Фев 06 2021 г., 03:05
 -- Версия сервера: 10.1.30-MariaDB
 -- Версия PHP: 7.3.2
 
@@ -25,6 +25,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Структура таблицы `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `id` int(11) NOT NULL,
+  `id_products` int(11) NOT NULL,
+  `name` mediumtext NOT NULL,
+  `feedback` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `feedbacks`
+--
+
+INSERT INTO `feedbacks` (`id`, `id_products`, `name`, `feedback`) VALUES
+(1, 1, 'M1', 'sdfgggggggggggggggggb'),
+(2, 1, 'Z22', 'svdfbgbngnnnnnnnnnnnnnn2'),
+(3, 0, '', ''),
+(4, 0, '', ''),
+(5, 0, '', ''),
+(10, 0, '', ''),
+(11, 0, '', ''),
+(12, 0, '', ''),
+(13, 0, '', ''),
+(14, 0, '', ''),
+(15, 0, '', ''),
+(16, 0, '', ''),
+(17, 0, '', ''),
+(18, 0, '', ''),
+(19, 0, '', ''),
+(20, 0, '', ''),
+(21, 0, '', ''),
+(22, 0, '', ''),
+(23, 1, '3', '3333333333 3333333'),
+(24, 0, '', ''),
+(25, 0, '', ''),
+(26, 0, '', ''),
+(27, 0, '', ''),
+(28, 0, '', ''),
+(29, 0, '', ''),
+(30, 0, '', ''),
+(31, 0, '', ''),
+(32, 0, '', '');
+
+-- --------------------------------------------------------
+
+--
 -- Структура таблицы `gallery`
 --
 
@@ -40,19 +87,71 @@ CREATE TABLE `gallery` (
 --
 
 INSERT INTO `gallery` (`id`, `image`, `size`, `countViews`) VALUES
-(1, '6016d838e1797.jpg', 2230017, 6),
-(2, '6016d88aab3b3.jpg', 4264559, 5),
+(1, '6016d838e1797.jpg', 2230017, 13),
+(2, '6016d88aab3b3.jpg', 4264559, 9),
 (4, '6016d8a27cc81.jpg', 2096522, 0),
 (5, '6016d8abb458f.jpg', 2467722, 0),
 (6, '6016d8b6ec192.jpg', 1194168, 0),
-(7, '6016d8c1c12ff.jpg', 3845060, 0),
+(7, '6016d8c1c12ff.jpg', 3845060, 1),
 (8, '6016d8d1f1634.jpg', 3449421, 1),
 (9, '6016d8dd9a691.jpg', 2940851, 1),
-(11, '60173bbc1215b.jpg', 1187692, 1);
+(11, '60173bbc1215b.jpg', 1187692, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `pictures`
+--
+
+CREATE TABLE `pictures` (
+  `id` int(11) NOT NULL,
+  `id_products` int(11) NOT NULL,
+  `image` mediumtext NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `pictures`
+--
+
+INSERT INTO `pictures` (`id`, `id_products`, `image`, `size`) VALUES
+(1, 1, '601d68aa11bf4.jpg', 2230017),
+(2, 1, '601d68ac8c97e.jpg', 4264559),
+(3, 1, '601d68af06517.jpg', 1187692),
+(4, 2, '601d6a67be1ae.jpg', 2940851);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` mediumtext NOT NULL,
+  `description` mediumtext NOT NULL,
+  `more_description` text NOT NULL,
+  `price` int(11) NOT NULL,
+  `sreated_at` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `more_description`, `price`, `sreated_at`) VALUES
+(1, 'Товар 1', 'Описание Товар 1', 'Полное описание Товар 1', 2200, '2021-02-05'),
+(2, 'Товар 2', 'Описание Товар 2', 'Полное Описание Товар 2', 3000, '2021-02-05');
 
 --
 -- Индексы сохранённых таблиц
 --
+
+--
+-- Индексы таблицы `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Индексы таблицы `gallery`
@@ -61,14 +160,44 @@ ALTER TABLE `gallery`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Индексы таблицы `pictures`
+--
+ALTER TABLE `pictures`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
+
+--
+-- AUTO_INCREMENT для таблицы `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT для таблицы `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT для таблицы `pictures`
+--
+ALTER TABLE `pictures`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT для таблицы `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
